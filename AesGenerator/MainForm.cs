3824490467bd1b535e9c3c6601687b02f846efdd
hetarex0834 <hetarex0834@gmail.com>
2023-02-02 19:38:33 +0900

    暗号化機能を作成

--- AesGenerator/MainForm.cs
+++ AesGenerator/MainForm.cs
@@ -1,16 +1,16 @@
 using System.Security.Cryptography;
+using System.Text;
 
 namespace AesGenerator
 {
     /// <summary>
-    /// AES-256暗号鍵を自動生成するアプリ
+    /// AES-256暗号を自動生成するアプリ
     /// </summary>
     public partial class MainForm : Form
     {
-        // 初期化ベクトル
-        private static readonly int iv = 12;
-        // 暗号鍵
-        private static readonly int key = 24;
+        // バイトサイズ
+        private static readonly int ivSize = 12; // 初期化ベクトル
+        private static readonly int keySize = 24; // 暗号鍵
 
         public MainForm() => InitializeComponent();
 
@@ -20,9 +20,11 @@ namespace AesGenerator
         private void BtnEncrypt_Click(object sender, EventArgs e)
         {
             // 初期化ベクトル
-            txtIv.Text = GenerateAes(iv);
+            txtIv.Text = GenerateAes(ivSize);
             // 暗号鍵
-            txtKey.Text = GenerateAes(key);
+            txtKey.Text = GenerateAes(keySize);
+            // 暗号化
+            txtCipher.Text = Encrypt(txtPlain.Text, txtIv.Text, txtKey.Text);
         }
 
         /// <summary>
@@ -36,15 +38,42 @@ namespace AesGenerator
         /// <summary>
         /// AESの自動生成
         /// </summary>
-        /// <param name="size">サイズ</param>
-        /// <returns></returns>
+        /// <param name="size">バイトサイズ</param>
+        /// <returns>Base64形式の文字列</returns>
         private static string GenerateAes(int size)
         {
-            var buf = new byte[size];
+            // 乱数生成
             using var rng = RandomNumberGenerator.Create();
-            rng.GetBytes(buf);
+            var bytes = new byte[size];
+            rng.GetBytes(bytes);
 
-            return Convert.ToBase64String(buf);
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// 平文をAES-256で暗号化
+        /// </summary>
+        /// <param name="plain">平文</param>
+        /// <param name="iv">初期化ベクトル</param>
+        /// <param name="key">暗号鍵</param>
+        /// <returns>暗号文</returns>
+        private static string Encrypt(string plain, string iv, string key)
+        {
+            // AES
+            using var aes = Aes.Create();
+            aes.BlockSize = 128; // ブロックサイズ
+            aes.KeySize = 256; // 鍵長
+            aes.Mode = CipherMode.CBC; // 暗号利用モード
+            aes.Padding = PaddingMode.PKCS7; // パディング
+            aes.IV = Encoding.UTF8.GetBytes(iv); // 初期化ベクトル
+            aes.Key = Encoding.UTF8.GetBytes(key); // 暗号鍵
+
+            // バイト配列化
+            var byteText = Encoding.UTF8.GetBytes(plain);
+            // 暗号化
+            var encryptText = aes.CreateEncryptor().TransformFinalBlock(byteText, 0, byteText.Length);
+
+            return Convert.ToBase64String(encryptText);
         }
     }
 }
